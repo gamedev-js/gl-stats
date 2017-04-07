4db661fce95fccf55c7dd66fc597aeccdadf587b
Johnny Wu <KarasAya@gmail.com>
2017-04-07 22:34:09 +0800

    add drawcalls

--- dist/gl-stats.dev.js
+++ dist/gl-stats.dev.js
@@ -18,12 +18,16 @@ let glifuncs = {
   // },
 
   drawArrays (original, name, ...args) {
-    ++this.__gli.totalDrawArraysCalls;
+    if (this.__gli) {
+      ++this.__gli.totalDrawArraysCalls;
+    }
     return original.apply(this, args);
   },
 
   drawElements (original, name, ...args) {
-    ++this.__gli.totalDrawElementsCalls;
+    if (this.__gli) {
+      ++this.__gli.totalDrawElementsCalls;
+    }
     return original.apply(this, args);
   },
 };
@@ -54,8 +58,19 @@ function bindWebGL() {
     WebGLRenderingContext.prototype[name] = _gliBind(name, WebGLRenderingContext.prototype[name], newFunc);
   }
 
+  WebGLRenderingContext.prototype.__reset = function () {
+    this.__gli.commands = [];
+    this.__gli.totalDrawArraysCalls = 0;
+    this.__gli.totalDrawElementsCalls = 0;
+  };
+
   WebGLRenderingContext.prototype.__inspect = function () {
+    if (this.__gli) {
+      return;
+    }
+
     let modules = {
+      commands: [],
       totalDrawArraysCalls: 0,
       totalDrawElementsCalls: 0,
     };
@@ -77,20 +92,107 @@ function bindWebGL() {
     this.__gli = modules;
     return modules;
   };
-
-  WebGLRenderingContext.prototype.gli = function () {
-    if ( this.__gli ) {
-      return this.__gli;
-    }
-    return this.__inspect();
-  };
 }
+
+let _css = `
+  .glstats {
+    position: absolute;
+    z-index: 9999;
+
+    padding: 5px;
+    width: 150px;
+    left: 5px;
+    top: 5px;
+
+    font-size: 10px;
+    font-family: 'Roboto Condensed', tahoma, sans-serif;
+    overflow: hidden;
+    user-select: none;
+    cursor: default;
+
+    background: rgba(0, 0, 0, 0.6);
+    border-radius: 3px;
+
+  }
+
+  .glstats-container {
+    display: flex;
+    flex-direction: column;
+    color: #888;
+    white-space: nowrap;
+  }
+
+  .glstats-item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: left;
+  }
+
+  .flex-1 {
+    flex: 1;
+  }
+
+  .right {
+    text-align: right;
+  }
+`;
+
+// add global style
+let styleEL = document.createElement('style');
+styleEL.type = 'text/css';
+styleEL.textContent = _css;
+document.head.appendChild(styleEL);
 
 bindWebGL();
 
+function _addItem(dom, name) {
+  let itemEL = document.createElement('div');
+  itemEL.className = 'glstats-item';
+
+  let spanName = document.createElement('span');
+  spanName.className = 'flex-1';
+  spanName.textContent = name;
+
+  let spanValue = document.createElement('span');
+  spanValue.className = 'flex-1 right';
+
+  let spanValueText = document.createTextNode('');
+  spanValueText.nodeValue = 'n/a';
+
+  itemEL.appendChild(spanName);
+  itemEL.appendChild(spanValue);
+  spanValue.appendChild(spanValueText);
+
+  dom.appendChild(itemEL);
+
+  return spanValueText;
+}
+
 class GLStats {
-  costructor(dom, opts = {}) {
-    // TODO
+  constructor(dom, opts = {}) {
+    this._root = document.createElement('div');
+    this._root.className = 'glstats';
+
+    let containerEL = document.createElement('div');
+    containerEL.className = 'glstats-container';
+
+    this._root.appendChild(containerEL);
+      this._drawcalls = _addItem(containerEL, 'Draw Calls');
+
+    dom.appendChild(this._root);
+  }
+
+  inspect (gl) {
+    gl.__inspect();
+    this._gl = gl;
+  }
+
+  reset () {
+    let gli = this._gl.__gli;
+    this._drawcalls.nodeValue = gli.totalDrawArraysCalls + gli.totalDrawElementsCalls;
+
+    this._gl.__reset();
   }
 }
 
