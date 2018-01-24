3b3191307dfef4f089e32107f78753cb75c8cd50
shay_chen <641278529@qq.com>
2018-01-24 10:09:35 +0800

    Upgrade rollup

--- script/rollup.config.js
+++ script/rollup.config.js
@@ -1,32 +1,48 @@
 'use strict';
 
+const buble = require('rollup-plugin-buble');
 const fsJetpack = require('fs-jetpack');
 const pjson = require('../package.json');
 
 let banner = `
 /*
  * ${pjson.name} v${pjson.version}
- * (c) ${new Date().getFullYear()} @Johnny Wu
+ * (c) ${new Date().getFullYear()} @gamedev-js
  * Released under the MIT License.
  */
 `;
 
 let dest = './dist';
 let file = 'gl-stats';
-let moduleName = 'GLStats';
+let name = 'GLStats';
+let sourcemap = true;
+let globals = {};
 
 // clear directory
 fsJetpack.dir(dest, { empty: true });
 
 module.exports = {
-  entry: './index.js',
-  targets: [
-    { dest: `${dest}/${file}.dev.js`, format: 'iife' },
-    { dest: `${dest}/${file}.js`, format: 'cjs' },
-  ],
-  moduleName,
-  banner,
+  input: './index.js',
   external: [],
-  globals: {},
-  sourceMap: true,
+  plugins: [
+    buble(),
+  ],
+  output: [
+    {
+      file: `${dest}/${file}.dev.js`,
+      format: 'iife',
+      name,
+      banner,
+      globals,
+      sourcemap
+    },
+    {
+      file: `${dest}/${file}.js`,
+      format: 'cjs',
+      name,
+      banner,
+      globals,
+      sourcemap
+    },
+  ],
 };
